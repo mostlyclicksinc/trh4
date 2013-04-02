1252ab4baab84ac961a6a701920ca7f93f126aa6
mostlyclicks <carlos@mostlyclicks.com>
2013-04-02 12:33:28 -0500

    nav edits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def selectified
+		#path_name = action_name + '_path'
+		hello = action_name + 'path'
+		'active' if current_page?(action_name + '_path')
+		
+	end
+
 end
